7521ef263765f54457592d34c5da7f41819967ee
Andrew Rae <ajrae.nv@gmail.com>
2022-01-17 10:53:20 -0500

    Adding reversible option to battery for 90 connectors.

--- src/footprints/bat.js
+++ src/footprints/bat.js
@@ -4,14 +4,28 @@ module.exports = {
         neg: 'neg'
     },
     params: {
-        class: 'PAD' // for Button
+        class: 'PAD', // for Button
+        reversible: false
     },
-    body: p => `
-
-    (module lib:bat (layer F.Cu) (tstamp 5BF2CC94)
-        ${p.at /* parametric position */}
-	  (pad 1 thru_hole circle (at 0 -1.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.net.pos.str})
-	  (pad 2 thru_hole circle (at 0 1.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.net.neg.str})
-     )
-    `
+    body: p => {
+      const header = `
+        (module lib:bat (layer F.Cu) (tstamp 5BF2CC94)
+            ${p.at /* parametric position */}
+        `
+      if (p.params.reversible) {
+        return `
+            ${header}
+            (pad 1 thru_hole circle (at 0 -3.24 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.net.pos.str})
+            (pad 2 thru_hole circle (at 0 0 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.net.neg.str})
+            (pad 3 thru_hole circle (at 0 3.24 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.net.pos.str}))
+        `
+      }
+      else {
+        return `
+            ${header}
+            (pad 1 thru_hole circle (at 0 -1.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.net.pos.str})
+            (pad 2 thru_hole circle (at 0 1.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.net.neg.str}))
+        `
+      }
+    }
 }

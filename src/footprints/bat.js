455988200fc6acf60ce5c7fea93c8499ad4e5bb1
Andrew Rae <ajrae.nv@gmail.com>
2022-01-19 17:31:28 -0500

    Made the battery footprint an actual 2mm apart for JST PH connector.

--- src/footprints/bat.js
+++ src/footprints/bat.js
@@ -10,13 +10,13 @@ module.exports = {
     body: p => {
         const pad = (num, text, net) => {
             if (num >= 2 && !p.param.reverse) return ''
-            const dist = 3.24
+            const dist = 2
             let x = p.param.reverse ? -dist : -dist/2;
             x += dist*num
             return `
                 (fp_text user "${text}" (at ${x} 1.8 ${p.rot}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15)) ))
                 (fp_text user "${text}" (at ${x} 1.8 ${p.rot}) (layer B.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15)) (justify mirror)))
-                (pad 1 thru_hole circle (at ${x} 0 ${p.rot}) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${net})
+                (pad 1 thru_hole roundrect (at ${x} 0 ${p.rot}) (size 1.50 2.25) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${net})
             `
         }
 

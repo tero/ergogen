3f43888ddc1cffa3c0e4e6c0b0c1d749b7309340
Andrew Rae <ajrae.nv@gmail.com>
2022-04-02 11:24:55 -0400

    Fixing short on battery pads

--- src/footprints/bat.js
+++ src/footprints/bat.js
@@ -8,7 +8,7 @@ module.exports = {
         reverse: false
     },
     body: p => {
-        const pad = (num, text, net) => {
+        const pad = (pnum, num, text, net) => {
             if (num >= 2 && !p.param.reverse) return ''
             const dist = 2
             let x = p.param.reverse ? -dist : -dist/2;
@@ -16,7 +16,7 @@ module.exports = {
             return `
                 (fp_text user "${text}" (at ${x} 1.8 ${p.rot}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15)) ))
                 (fp_text user "${text}" (at ${x} 1.8 ${p.rot}) (layer B.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15)) (justify mirror)))
-                (pad 1 thru_hole roundrect (at ${x} 0 ${p.rot}) (size 1.50 2.25) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${net})
+                (pad ${pnum} thru_hole roundrect (at ${x} 0 ${p.rot}) (size 1.50 2.25) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${net})
             `
         }
 
@@ -29,9 +29,9 @@ module.exports = {
                 (fp_text value "" (at 0 0) (layer F.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
 
                 ${''/* battery pads */}
-                ${pad(0, 'B+', p.net.pos.str)}
-                ${pad(1, 'B-', p.net.neg.str)}
-                ${pad(2, 'B+', p.net.pos.str)}
+                ${pad(1, 0, 'B+', p.net.pos.str)}
+                ${pad(2, 1, 'B-', p.net.neg.str)}
+                ${pad(1, 2, 'B+', p.net.pos.str)}
             )
         `
     }
